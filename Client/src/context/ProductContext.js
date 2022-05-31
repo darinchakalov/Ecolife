53838ebada06bc7589c195db7af6bf508c5a227a
darinchakalov <darinchakalov@hotmail.com>
2022-05-31 15:55:11 +0300

    products state persist on refresh

--- Client/src/context/ProductContext.js
+++ Client/src/context/ProductContext.js
@@ -1,4 +1,5 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import useLocalStorage from "../hooks/useLocalStorage.js";
 export const ProductContext = createContext();
 
 const initialState = {
@@ -8,7 +9,10 @@ const initialState = {
 const reducer = (state, action) => {
 	switch (action.type) {
 		case "ADD_NEW_PRODUCT":
-			return { ...state, items: [...state.items, { product: action.payload, productCount: action.quantity }] };
+			return {
+				...state,
+				items: [...state.items, { product: action.payload, productCount: action.quantity }],
+			};
 		case "REMOVE_PRODUCT":
 			return state.items.filter((product) => product !== action.payload);
 
@@ -19,6 +23,10 @@ const reducer = (state, action) => {
 
 export const ProductProvider = ({ children }) => {
 	const [products, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(() => {
+		localStorage.setItem("items", JSON.stringify(products));
+	}, [products]);
 
 	const addProduct = (product, quantity) => {
 		dispatch({
