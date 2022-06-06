da08abe82db2f06a85daadcf9e1c9b7c6778c2e7
darinchakalov <darinchakalov@hotmail.com>
2022-06-06 11:24:18 +0300

    contact form validation and functionality

--- Client/src/components/Contacts/ContactForm/ContactForm.js
+++ Client/src/components/Contacts/ContactForm/ContactForm.js
@@ -1,6 +1,34 @@
 import "./ContactForm.css";
+import * as notificationService from "../../../services/notificationService.js";
+import { useNavigate } from "react-router-dom";
 
 export default function ContactForm() {
+	let navigate = useNavigate();
+
+	const contactFormSubmitHandler = (e) => {
+		e.preventDefault();
+
+		const { name, email, message } = Object.fromEntries(new FormData(e.target));
+
+		if (name.length < 4 || name.length > 12) {
+			return notificationService.warning("Name should be between 4 and 12 characters");
+		}
+
+		let emailValidationRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+		if (!emailValidationRegex.test(email)) {
+			return notificationService.warning("Please use a valid email address");
+		}
+
+		if (message.length < 10 || message.length > 100) {
+			return notificationService.warning("The message should be between 10 and 100 characters");
+		}
+
+		notificationService.success("Message was send successfully!");
+		setTimeout(() => {
+			navigate("/");
+		}, 2000);
+	};
+
 	return (
 		<div className="contact-form-page-wrapper">
 			<div className="contact-info-wrapper">
@@ -17,16 +45,14 @@ export default function ContactForm() {
 			</div>
 			<div className="contact-form-wrapper">
 				<h2>Contact Us</h2>
-				<form className="contact-form">
+				<form className="contact-form" onSubmit={contactFormSubmitHandler}>
 					<label htmlFor="name">Name</label>
-					<input type="name" name="" placeholder="Ivan" />
+					<input type="text" name="name" placeholder="Ivan" />
 					<label htmlFor="email">Email</label>
-					<input type="email" name="" placeholder="ivan@abv.bg" />
+					<input type="email" name="email" placeholder="ivan@abv.bg" />
 					<label htmlFor="message">Message</label>
 					<textarea name="message" rows="10" cols=""></textarea>
-					<button className="submit-button" type="submit">
-						Submit
-					</button>
+					<input className="contact-form-submit-button" type="submit" value="Submit" />
 				</form>
 			</div>
 		</div>
